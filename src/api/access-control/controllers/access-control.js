417ddab051c91a917eaadd004a014e3801b229b3
PuaZhiXian <xianpua2001@gmail.com>
2023-08-12 03:00:52 +0800

    customize find all access control collection

--- src/api/access-control/controllers/access-control.js
+++ src/api/access-control/controllers/access-control.js
@@ -1,9 +1,30 @@
 'use strict';
 
+
+const {sanitize} = require('@strapi/utils')
+const {contentAPI} = sanitize;
 /**
  * access-control controller
  */
+const {createCoreController} = require('@strapi/strapi').factories;
+const jwt = require('jsonwebtoken');
+module.exports = createCoreController('api::access-control.access-control', ({strapi}) => ({
 
-const { createCoreController } = require('@strapi/strapi').factories;
+  async find(ctx) {
+    const decoded  =  jwt.decode(ctx.request.header.authorization)
+    const vendor_id =  decoded.id;
 
-module.exports = createCoreController('api::access-control.access-control');
+    ctx.request.query.filters = {
+      vendor_id: {
+        id: {
+          $eq: vendor_id
+        },
+      }
+    }
+
+    const contentType = strapi.contentType('api::access-control.access-control')
+    const sanitizedQueryParams = await contentAPI.query(ctx.query, contentType)
+    const entities = await strapi.entityService.findMany(contentType.uid, sanitizedQueryParams)
+    return await contentAPI.output(entities, contentType);
+  }
+}));
--- src/api/access-control/routes/access-control.js
+++ src/api/access-control/routes/access-control.js
@@ -6,4 +6,23 @@
 
 const { createCoreRouter } = require('@strapi/strapi').factories;
 
-module.exports = createCoreRouter('api::access-control.access-control');
+module.exports = createCoreRouter('api::access-control.access-control', {
+  prefix: '',
+  only: ['find', 'findOne'],
+  except: [],
+  config: {
+    find: {
+      auth: false,
+      policies: [],
+      middlewares: [],
+    },
+    findOne: {
+      auth: false,
+      policies: [],
+      middlewares: [],
+    },
+    create: {},
+    update: {},
+    delete: {},
+  },
+});
