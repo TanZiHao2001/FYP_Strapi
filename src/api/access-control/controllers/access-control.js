70fb9a9a5cb6fb1dd78d37c90f8341fe27ca9bf4
PuaZhiXian <xianpua2001@gmail.com>
2023-08-13 04:08:07 +0800

    get subscribed api collection overviews

--- src/api/access-control/controllers/access-control.js
+++ src/api/access-control/controllers/access-control.js
@@ -6,25 +6,43 @@
 const {createCoreController} = require('@strapi/strapi').factories;
 const jwt = require('jsonwebtoken');
 const {sanitize} = require('@strapi/utils')
+const {removeUserRelationFromRoleEntities} = require("@strapi/plugin-users-permissions/server/utils/sanitize/visitors");
 const {contentAPI} = sanitize;
 
 module.exports = createCoreController('api::access-control.access-control', ({strapi}) => ({
 
   async find(ctx) {
-    const decoded  =  jwt.decode(ctx.request.header.authorization)
-    const vendor_id =  decoded.id;
+    const decoded = jwt.decode(ctx.request.header.authorization)
+    const vendor_id = decoded.id;
 
-    ctx.request.query.filters = {
-      vendor_id: {
-        id: {
-          $eq: vendor_id
+    ctx.request.query = {
+      filters: {
+        vendor_id: {
+          id: {
+            $eq: vendor_id
+          },
+        }
+      },
+      fields: ['status'],
+      populate: {
+        api_collection_id: {
+          fields: ['api_collection_name']
         },
       }
     }
-
+    console.log(ctx.request)
     const contentType = strapi.contentType('api::access-control.access-control')
     const sanitizedQueryParams = await contentAPI.query(ctx.query, contentType)
     const entities = await strapi.entityService.findMany(contentType.uid, sanitizedQueryParams)
-    return await contentAPI.output(entities, contentType);
+    const result = await contentAPI.output(entities, contentType);
+
+
+    result.forEach(item => {
+      item.api_collection_name = item.api_collection_id.api_collection_name;
+      item.api_collection_id = item.api_collection_id.id;
+    });
+
+    return result;
+
   }
 }));
