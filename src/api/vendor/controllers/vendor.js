7da1ea301bb02b7f899b5310dca45a9b7492d734
PuaZhiXian <xianpua2001@gmail.com>
2023-08-14 05:35:22 +0800

    change login in response type

--- src/api/vendor/controllers/vendor.js
+++ src/api/vendor/controllers/vendor.js
@@ -6,7 +6,7 @@
 
 const {sanitize} = require('@strapi/utils')
 const {contentAPI} = sanitize;
-const { createCoreController } = require('@strapi/strapi').factories;
+const {createCoreController} = require('@strapi/strapi').factories;
 
 // module.exports = createCoreController('api::vendor.vendor', ({strapi}) => ({
 //     async vendorLogin(ctx) {
@@ -21,39 +21,43 @@ const { createCoreController } = require('@strapi/strapi').factories;
 const bcrypt = require('bcryptjs');
 const jwt = require('jsonwebtoken');
 
-module.exports = createCoreController('api::vendor.vendor', ({ strapi }) => ({
-    async vendorLogin(ctx) {
-        try{
-        const { email, password } = ctx.request.body;
-        
-        ctx.request.query.filters = {
-            email: {
-                $eq: email
-            }
+module.exports = createCoreController('api::vendor.vendor', ({strapi}) => ({
+  async vendorLogin(ctx) {
+    try {
+      const {email, password} = ctx.request.body;
+
+      ctx.request.query.filters = {
+        email: {
+          $eq: email
         }
-        
-        const contentType = strapi.contentType('api::vendor.vendor')
-        const sanitizedQueryParams = await contentAPI.query(ctx.query, contentType)
-        const entities = await strapi.entityService.findMany(contentType.uid, sanitizedQueryParams)
-        if(entities.length === 0){
-            throw new Error('Vendor not found!');
-        }
-        const isPasswordValid = await bcrypt.compare(password, entities[0].password);
-        if (!isPasswordValid) {
-            throw new Error('Invalid password');
-        }
-        const token = jwt.sign({ id: entities[0].id, date: new Date()}, 'your-secret-key', {
-            expiresIn: '1d', 
-        });
-        console.log(token);
-        // console.log(parseJwt(token));
-        console.log(jwt.decode(token));
-        ctx.send({ token, entities });
-        console.log(entities[0].password);
-        return entities;
-        } catch(err) {
-            ctx.send({ error: err.message }, 400);
-        }
-    } 
+      }
+
+      const contentType = strapi.contentType('api::vendor.vendor')
+      const sanitizedQueryParams = await contentAPI.query(ctx.query, contentType)
+      const entities = await strapi.entityService.findMany(contentType.uid, sanitizedQueryParams)
+      if (entities.length === 0) {
+        return {error: 'Email not found!'};
+      }
+      const isPasswordValid = await bcrypt.compare(password, entities[0].password);
+      if (!isPasswordValid) {
+        return {error: 'Invalid password !'};
+      }
+      const token = jwt.sign({id: entities[0].id, date: new Date()}, 'your-secret-key', {
+        expiresIn: '1d',
+      });
+      // console.log(token);
+      // console.log(parseJwt(token));
+      // console.log(jwt.decode(token));
+      // ctx.send({ token, entities });
+      // console.log(entities[0].password);
+      // return entities;
+      const result = {
+        token: token
+      }
+      return result;
+    } catch (err) {
+      ctx.send({error: err.message}, 400);
+    }
+  }
 
 }));
