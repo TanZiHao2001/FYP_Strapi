1de455d432e81bd4e1d087dca4622f39bf2b4832
Tan Zi Hao <u2005328@siswa.um.edu.my>
2023-08-31 04:26:37 +0800

    api log

--- src/api/api-log/controllers/api-log.js
+++ src/api/api-log/controllers/api-log.js
@@ -1,9 +1,33 @@
-'use strict';
+"use strict";
 
 /**
  * api-log controller
  */
 
-const { createCoreController } = require('@strapi/strapi').factories;
+const { createCoreController } = require("@strapi/strapi").factories;
 
-module.exports = createCoreController('api::api-log.api-log');
+module.exports = createCoreController("api::api-log.api-log", ({ strapi }) => ({
+  async create(ctx) {
+    const malaysiaTimeZoneOffset = 8; // Malaysia time is UTC+8
+
+    const currentDate = new Date();
+    const malaysiaTime = new Date(
+      currentDate.getTime() + malaysiaTimeZoneOffset * 60 * 60 * 1000
+    );
+
+    const isoFormattedDate = malaysiaTime.toISOString();
+
+
+    const entry = await strapi.entityService.create("api::api-log.api-log", {
+      data: {
+        time: isoFormattedDate,
+        response_time: 2,
+        response_size: 2,
+        status: "success",
+        publishedAt: Date.now(),
+      },
+    });
+
+    ctx.send({message: "Added API log"})
+  },
+}));
--- src/api/api-log/routes/api-log.js
+++ src/api/api-log/routes/api-log.js
@@ -1,9 +1,28 @@
-'use strict';
+"use strict";
 
 /**
  * api-log router
  */
 
-const { createCoreRouter } = require('@strapi/strapi').factories;
+const { createCoreRouter } = require("@strapi/strapi").factories;
 
-module.exports = createCoreRouter('api::api-log.api-log');
+module.exports = createCoreRouter("api::api-log.api-log", {
+  prefix: "",
+  only: ["find", "findOne", "create"],
+  except: [],
+  config: {
+    find: {
+      auth: false,
+      policies: [],
+      middlewares: [],
+    },
+    findOne: {},
+    create: {
+      auth: false,
+      policies: [],
+      middlewares: [],
+    },
+    update: {},
+    delete: {},
+  },
+});
