6314e3eceade360f863786009211fb9373c6cd92
Tan Zi Hao <u2005328@siswa.um.edu.my>
2023-12-05 21:56:45 +0800

    sort the announcement list

--- src/api/announcement/controllers/CustomAnnouncement.js
+++ src/api/announcement/controllers/CustomAnnouncement.js
@@ -9,7 +9,7 @@ module.exports = {
     getAnnouncementList: async (ctx) => {
       try {
         ctx.request.query = {
-            fields: ["isActive", "title", "createdAt", "updatedAt"],
+            fields: ["title", "startDate", "endDate"],
             publicationState: 'live',
         };
   
@@ -28,9 +28,43 @@ module.exports = {
         );
 
         const result = await contentAPI.output(entities, contentType);
-        return result;
+          
+        const tempResult = groupByDate(result);
+        
+        tempResult.active.sort((a, b) => new Date(a.endDate) - new Date(b.endDate));
+        tempResult.upcoming.sort((a, b) => new Date(a.startDate) - new Date(b.startDate));
+        tempResult.expired.sort((a, b) => new Date(b.endDate) - new Date(a.endDate));
+
+        const finalResult = [...tempResult.active, ...tempResult.upcoming, ...tempResult.expired].map(({ id, title, startDate, endDate }) => ({
+            id,
+            title,
+            startDate,
+            endDate
+        }));
+        return finalResult;
       } catch (error) {
         errorHandler(ctx, error)
       }
     }
   };
+
+  function groupByDate(arr){
+    const currentTime = new Date();
+    let result = {
+        active: [],
+        upcoming: [],
+        expired: []
+    };
+
+    arr.forEach(item => {
+        if(currentTime - new Date(item.startDate) >= 0 && new Date(item.endDate) - currentTime >= 0){
+            result.active.push(item);
+        } else if(currentTime - new Date(item.startDate) < 0) {
+            result.upcoming.push(item);
+        } else if(currentTime - new Date(item.startDate) >= 0 && new Date(item.endDate) - currentTime <= 0){
+            result.expired.push(item);
+        }
+    })
+
+    return result;
+  }
