0c4dacbb1b2122ca64e718b20671cf619a4069d8
Tan Zi Hao <u2005328@siswa.um.edu.my>
2024-01-08 03:12:33 +0800

    debugging annoucnement

--- src/api/announcement/controllers/CustomAnnouncement.js
+++ src/api/announcement/controllers/CustomAnnouncement.js
@@ -130,8 +130,9 @@ module.exports = {
                         // };
                         // check if startDate for current announcement matches the current checking date 
                         console.log("Curr Month Date k " + currMonthDate[k])
+                        console.log("all year month date " + startYear + " " + startMonth + " " + startDate + " " + endYear + " " + endMonth + " " + endDate)
                         console.log("Year Month Date of it")
-                        console.log(currMonthDate[k].getFullYear() + " " + currMonthDate[k].getMonth + " " + currMonthDate[k].getDate());
+                        console.log(currMonthDate[k].getFullYear() + " " + currMonthDate[k].getMonth() + " " + currMonthDate[k].getDate());
                         if(startYear === new Date(currMonthDate[k]).getFullYear() && startMonth === new Date(currMonthDate[k]).getMonth() && startDate === new Date(currMonthDate[k]).getDate()) {
                             console.log("this announcement equals ")
                             let rowIndex = formattedDates.findIndex(
